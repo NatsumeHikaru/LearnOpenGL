b010305c46891b4f1e3b29312d6f196b3d461b00
Hikaru Natsume <cqhac@qq.com>
2022-08-22 19:10:08 +0800

    fix bug

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,34 +1,24 @@
-#include<GL/glew.h>
 #include<GLFW/glfw3.h>
-#include<iostream>
 
 int main() {
 	GLFWwindow* window;
 
 	if (!glfwInit()) return -1;
 
-	window = glfwCreateWindow(640, 480, "Hello world", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);
-	
-	if (glewInit() != GLEW_OK) std::cout << "Error!" << std::endl; // 需要有效的上下文窗口
 
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
-
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
+	
 	glfwTerminate();
 	return 0;
 }
