c5b42c674a97ce29d4c74c08a86db4d2bf53928a
Natsume Hikaru <cqhac@qq.com>
2022-09-07 16:10:36 +0800

    set up a test framework

--- main/OpenGL/src/Application.cpp
+++ main/OpenGL/src/Application.cpp
@@ -20,6 +20,8 @@
 #include"imgui/imgui.h"
 #include"imgui/imgui_impl_glfw_gl3.h"
 
+#include"tests/TestClearColor.h"
+
 int main() {
 	if (!glfwInit()) return -1;
 
@@ -38,51 +40,8 @@ int main() {
 	glfwSwapInterval(1);
 	if (glewInit() != GLEW_OK) std::cout << "Error!" << std::endl;
 
-	float positions[] = {
-		-50.0f, -50.0f, 0.0f, 0.0f, // 0
-		 50.0f, -50.0f, 1.0f, 0.0f, // 1
-		 50.0f,  50.0f, 1.0f, 1.0f, // 2
-	    -50.0f,  50.0f, 0.0f, 1.0f, // 3
-	};
-
-	unsigned int indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
 	GLCall(glEnable(GL_BLEND));
 	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-	unsigned int vao;
-	GLCall(glGenVertexArrays(1, &vao));
-	GLCall(glBindVertexArray(vao));
-
-	VertexArray* va = new VertexArray();
-	
-	VertexBuffer *vb = new VertexBuffer(positions, sizeof(positions));
-
-	VertexBufferLayout* layout = new VertexBufferLayout();
-	layout->Push<float>(2);
-	layout->Push<float>(2);
-	va->AddBuffer(*vb, *layout);
-
-	IndexBuffer* ib = new IndexBuffer(indices, 6);
-
-	glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0), glm::vec3(0, 0, 0));
-
-	Shader* shader = new Shader("res/shader/basic.shader");
-	shader->Bind();
-
-
-	Texture* texture = new Texture("res/textures/kazuha.png");
-	texture->Bind();
-	shader->SetUniform1i("u_Texture", 0);
-
-	va->unBind();
-	vb->unBind();
-	va->unBind();
-	shader->unBind();
 
 	Renderer* renderer = new Renderer();
 
@@ -90,39 +49,16 @@ int main() {
 	ImGui_ImplGlfwGL3_Init(window, true);
 	ImGui::StyleColorsDark();
 
-	glm::vec3 translationA(200, 200, 0);
-	glm::vec3 translationB(400, 200, 0);
+	test::TestClearColor* test = new test::TestClearColor();
 
-	float r = 0.0f;
-	float delta = 0.05f;
 	while (!glfwWindowShouldClose(window)) {
 		renderer->Clear();
 
+		test->OnUpdate(0.0f);
+		test->OnRender();
+
 		ImGui_ImplGlfwGL3_NewFrame();
-
-		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0), translationA);
-			glm::mat4 mvp = proj * view * model;
-			shader->Bind();
-			shader->SetUniformMat4f("u_MVP", mvp);
-			renderer->Draw(*va, *ib, *shader);
-		}
-
-		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0), translationB);
-			glm::mat4 mvp = proj * view * model;
-			shader->Bind();
-			shader->SetUniformMat4f("u_MVP", mvp);
-			renderer->Draw(*va, *ib, *shader);
-		}
-
-		// demo
-		{
-			ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f); 
-			ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
-
+		test->OnImGuiRender();
 		ImGui::Render();
 		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
@@ -130,7 +66,8 @@ int main() {
 		glfwPollEvents();
 	}
 
-	delete va, vb, ib, shader, texture;
+	//delete va, vb, ib, shader, texture;
+	delete renderer, test;
 
 	ImGui_ImplGlfwGL3_Shutdown();
 	ImGui::DestroyContext();
